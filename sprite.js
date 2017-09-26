9ca36e50cf33820167039ee6e43455d56c4d31aa
wisniewski94 <lion7942@gmail.com>
2017-09-27 01:40:12 +0200

    Changed dir

--- sprite.js
+++ sprite.js
@@ -1,17 +1,6 @@
 'use strict';
 
 function Sprite(opt) {
-    //config
-    var index   = 0,
-        frame   = -1,
-        time    = null,
-        playing = false,
-        err     = opt.err,
-        playErr = new Error('Animation is already playing'),
-        rangeErr= new RangeError('Parameter must be between 1 and ' + frames),
-        missing = new Error('Missing property'),
-        rAF, rAF2, count, last, paused;
-    
     //private settings
     var cvs     = document.getElementById(opt.id),
         x       = cvs.width = opt.width,
@@ -20,25 +9,27 @@ function Sprite(opt) {
         img     = new Image(),
         frames  = opt.image_width ? (opt.image_width / x) : (function (){throw missing}());
     
+    //config
+    var index   = 0,
+        frame   = -1,
+        time    = null,
+        playing = false,
+        err     = opt.err,
+        playErr = new Error('Animation is already playing'),
+        rangeErr= console.error(new RangeError('Parameter must be between 1 and ' + frames)),
+        missing = new Error('Missing property'),
+        rAF, count, paused;
+    
     img.src = opt.src;
     ctx.drawImage(img, index * x, 0, x, y, 0, 0, x, y);
 
-    this.loop = function (fps, dir, n) {
-        if (playing && err) throw playErr;
-        if (fps == null) fps = 60;
-        if (n !== null) count = 0;
-        var delay = 1000 / fps;
-        rAF = requestAnimationFrame(anim.bind(null, n, delay, dir));
-        playing = true;
-    };
-
     this.play = function (e) {
         if (playing && err) throw playErr;
-        if (e.to > frames && err) throw rangeErr;
+        if (e.to > frames && err) rangeErr;
         if (e.from < 1 && err) throw rangeErr;
         if (e.fps == null) e.fps = 60;
-        if (e.from == null) e.from = 0;
-        if (e.to == null) e.to = frames - 1;
+        if (e.from == null) e.from = 1;
+        if (e.to == null) e.to = frames;
         if (e.n == null) e.n = 1;
         if (paused == null || !paused || count !== count || count == null) {
             if (e.to > e.from) count = 0;
@@ -49,14 +40,12 @@ function Sprite(opt) {
         e.from--;
         e.to--;
         playing = true;
-        rAF2 = requestAnimationFrame(play.bind(null, e.from, e.to, e.n, delay));
-        e.callback();
+        rAF = requestAnimationFrame(play.bind(null, e.from, e.to, e.n, delay, e.step, e.loop));
     }
     
     this.pause = function () {
         if (!playing && err) throw playErr;
         cancelAnimationFrame(rAF);
-        cancelAnimationFrame(rAF2);
         playing = false;
         paused = true;
         frame = -1;
@@ -65,7 +54,6 @@ function Sprite(opt) {
     this.reset = function (to) {
         if (to == null) to = 0;
         cancelAnimationFrame(rAF);
-        cancelAnimationFrame(rAF2);
         playing = false;
         frame = -1;
         index = to - 1;
@@ -74,55 +62,16 @@ function Sprite(opt) {
     };
     
     this.frame = function() {
-        return index + 1;    
+        return index + 1;
     };
     
-    var anim = function (n, delay, dir, timestamp) {
-        if (time == null) time = timestamp;
-        rAF = requestAnimationFrame(anim.bind(null, n, delay, dir));
-        var current = Math.floor((timestamp - time) / delay);
-        if (current > frame) {
-            if (index == frames - 1) count++;
-            frame = current;
-            ctx.clearRect(0, 0, x, y);
-            ctx.drawImage(img, index * x, 0, x, y, 0, 0, x, y);
-            if (dir == 'backward') {
-                if (index <= 0) {
-                    index = frames;
-                }
-                index -= 1;
-            } else {
-                if (index >= frames - 1) {
-                    index = 0;
-                } else {
-                    index++;
-                }
-            }//if dir backward
-        }//if current> frame
-
-        if (typeof n !== null && n > 0) {
-            if (count == n && index == 1 && dir !== 'backward') {
-                playing = false;
-                paused = false;
-                cancelAnimationFrame(rAF);
-            }//if
-            if (count == n && index == frames - 1 && dir == 'backward') {
-                playing = false;
-                paused = false;
-                cancelAnimationFrame(rAF);
-            }//if
-        }//if typeof
-
-    };//anim
-    
-    var play = function (from, to, n, delay, timestamp) {
-        rAF2 = requestAnimationFrame(play.bind(null, from, to, n, delay));
+    var play = function (from, to, n, delay, callback1, callback2, timestamp) {
+        rAF = requestAnimationFrame(play.bind(null, from, to, n, delay, callback1, callback2));
         if (time === null) time = timestamp;
         var current = Math.floor((timestamp - time) / delay);
         if (current > frame) {
             frame = current;
-            if (count >= n && index == last && n !== 0) {
-                cancelAnimationFrame(rAF2);
+            if (count >= n && index == to && n !== 0) {
                 playing = false;
                 paused = false;
             }
@@ -130,22 +79,31 @@ function Sprite(opt) {
             ctx.drawImage(img, index * x, 0, x, y, 0, 0, x, y);
             if (to > from) {
                 if (index >= to) {
+                    typeof callback1 === 'function' && callback1(index + 1);
                     index = from;
                 } else {
+                    typeof callback1 === 'function' && callback1(index + 1);
                     index++;
                 }
-                last = to;
-                if (index == to) count++;
+                if (index == to) {
+                    typeof callback2 === 'function' && callback2(count);
+                    count++;
+                }
             } else if (from > to) {
                 if (index <= to) {
+                    typeof callback1 === 'function' && callback1(index + 1);                    
                     index = from;
                 } else {
+                    typeof callback1 === 'function' && callback1(index + 1);
                     index--;
                 }
-                last = to;
-                if (index == from) count++;
+                if (index == from) {
+                    typeof callback2 === 'function' && callback2(count);
+                    count++;
+                }
             }// else if
 
         } //current > frame
+        
     };
 }
